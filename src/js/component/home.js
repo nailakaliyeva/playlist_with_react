dbd7e4fac07e206ae7fbe2d2031f82b8333573cc
Naila <nailakaliyeva@gmail.com>
2019-07-26 22:15:01 +0000

    created function playSong

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -13,16 +13,25 @@ export class Home extends React.Component {
 			.then(resp => resp.json())
 			.then(songs => this.setState({ songs }));
 	}
+
+	playSong(i) {
+		document.querySelector("#audio" + i).play();
+	}
+
 	render() {
 		return (
 			<ul className="list-group mx-1">
 				{this.state.songs.map((item, index) => {
 					return (
 						<div key={index}>
-							<li className="list-group-item m-1">
+							<li
+								className="list-group-item m-1"
+								onClick={() => this.playSong(index)}>
 								{item.name}
 								<audio
+									id={"audio" + index}
 									type="audio/mp3"
+									ref={this.music}
 									src={
 										"https://assets.breatheco.de/apis/sound/" +
 										this.state.songs[index].url
