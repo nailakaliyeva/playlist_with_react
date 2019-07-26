13dd0d88f97679d238bb312a5aaaefda3990c7cc
Naila <nailakaliyeva@gmail.com>
2019-07-26 20:17:00 +0000

    fetched the information and gave it some style. Most important thing to remember: json format doesnt render&return objects

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,26 +1,29 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			songs: []
+		};
+	}
+	componentDidMount() {
+		// this.pauseBtn.style.display = "none";
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then(resp => resp.json())
+			.then(songs => this.setState({ songs }));
+	}
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
-			</div>
+			<ul className="list-group mx-1">
+				{this.state.songs.map((item, index) => {
+					return (
+						<div key={index}>
+							<li className="list-group-item m-1">{item.name}</li>
+						</div>
+					);
+				})}
+			</ul>
 		);
 	}
 }
